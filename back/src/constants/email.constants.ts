b19f90d4636d7e9eaf171a1f7a7d71faa40f60c5
Santiago Dylan Terenziani <sterenziani@itba.edu.ar>
2023-09-08 22:48:00 -0300

    Chore: Refactored email translation files

---
+++ back/src/constants/email.constants.ts
@@ -0,0 +1,25 @@
+export const TRANSLATIONS: {[key:string]:any} = {
+    resources : {
+        "en" : {
+            translation : {
+                "welcome": "Welcome to AutoScheduler!",
+                "howToVerify": "Thank you for joining AutoScheduler! To make your university visible to your students, please write to us at {{verificationEmail}} with proof that you represent {{universityName}} so we can begin the verification process.",
+                "resetYourPasswordSubject": "Reset your AutoScheduler password",
+                "resetYourPassword": "Reset Your Password",
+                "clickLinkBelow": "Did you forget your password? If so, just click on the link below to pick a new password! Do keep in mind the link will expire in 3 days.",
+                "clickHereToReset": "Click here to reset your password."
+            }
+        },
+        "es" : {
+            translation: {
+                "welcome": "¡Bienvenido a AutoScheduler!",
+                "howToVerify": "Gracias por unirse a AutoScheduler. Para hacer que su universidad sea visible a sus estudiantes, por favor escríbanos a {{verificationEmail}} con documentación que demuestre su vínculo con {{universityName}} para que podamos comenzar el proceso de verificación.",
+                "resetYourPasswordSubject": "Restablezca su contraseña de AutoScheduler",
+                "resetYourPassword": "Restablezca su Contraseña",
+                "clickLinkBelow": "¿Olvidó su contraseña? Si es así, haga click sobre el siguiente link para elegir una nueva contraseña. Tenga en cuenta que este link expirará en 3 días.",
+                "clickHereToReset": "Haga click aquí para cambiar su contraseña."
+            }
+        }
+    },
+    lng : "en"
+}
--- back/src/services/email.service.ts
+++ back/src/services/email.service.ts
@@ -1,30 +1,5 @@
+import { TRANSLATIONS } from '../constants/email.constants';
 import nodemailer from 'nodemailer';
-
-const translations: {[key:string]:any} = {
-    resources : {
-        "en" : {
-            translation : {
-                "welcome": "Welcome to AutoScheduler!",
-                "howToVerify": "Thank you for joining AutoScheduler! To make your university visible to your students, please write to us at {{verificationEmail}} with proof that you represent {{universityName}} so we can begin the verification process.",
-                "resetYourPasswordSubject": "Reset your AutoScheduler password",
-                "resetYourPassword": "Reset Your Password",
-                "clickLinkBelow": "Did you forget your password? If so, just click on the link below to pick a new password! Do keep in mind the link will expire in 3 days.",
-                "clickHereToReset": "Click here to reset your password."
-            }
-        },
-        "es" : {
-            translation: {
-                "welcome": "¡Bienvenido a AutoScheduler!",
-                "howToVerify": "Gracias por unirse a AutoScheduler. Para hacer que su universidad sea visible a sus estudiantes, por favor escríbanos a {{verificationEmail}} con documentación que demuestre su vínculo con {{universityName}} para que podamos comenzar el proceso de verificación.",
-                "resetYourPasswordSubject": "Restablezca su contraseña de AutoScheduler",
-                "resetYourPassword": "Restablezca su Contraseña",
-                "clickLinkBelow": "¿Olvidó su contraseña? Si es así, haga click sobre el siguiente link para elegir una nueva contraseña. Tenga en cuenta que este link expirará en 3 días.",
-                "clickHereToReset": "Haga click aquí para cambiar su contraseña."
-            }
-        }
-    },
-    lng : "en"
-}
 
 export default class EmailService {
     private static instance: EmailService;
@@ -54,8 +29,7 @@ export default class EmailService {
         });
 
         const i18next = require("i18next");
-        i18next.init(translations);
-
+        i18next.init(TRANSLATIONS);
         const HandlebarsI18n = require("handlebars-i18n");
         HandlebarsI18n.init();
 
@@ -102,9 +76,9 @@ export default class EmailService {
 
     async sendUniversityWelcomeEmail(destination: string, universityName: string, locale: string|undefined): Promise<void> {
         const template = "welcome";
-        const emailLanguage = (locale && translations.resources[locale])? locale:translations.lng;
-        const subject = translations.resources[emailLanguage].translation.welcome;
-        
+        const emailLanguage = this.getAvailableLocale(locale);
+        const subject = TRANSLATIONS.resources[emailLanguage].translation.welcome;
+
         const context = {
             verificationEmail: process.env.EMAIL_VERIFICATION_ADDRESS,
             universityName: universityName,
@@ -115,10 +89,14 @@ export default class EmailService {
 
     async sendPasswordResetEmail(destination: string, internalResetPath: string, locale: string|undefined): Promise<void> {
         const template = "resetPassword";
-        const emailLanguage = (locale && translations.resources[locale])? locale:translations.lng;
-        const subject = translations.resources[emailLanguage].translation.resetYourPasswordSubject;
+        const emailLanguage = this.getAvailableLocale(locale);
+        const subject = TRANSLATIONS.resources[emailLanguage].translation.resetYourPasswordSubject;
 
         const context = { link: process.env.BASE_URL+"/"+internalResetPath, locale: emailLanguage };
         this.sendEmailTemplate(destination, subject, template, context);
+    }
+
+    private getAvailableLocale(locale: string|undefined){
+        return (locale && TRANSLATIONS.resources[locale])? locale:TRANSLATIONS.lng
     }
 }
